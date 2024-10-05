9282541779e380e5c83f2ba13bc7bbe18ccb30ca
Ilja <100518672+diliny867@users.noreply.github.com>
2024-10-05 20:22:23 +0300

    Refactor: remove raster_data struct

--- Raster To ASCII/image_raster.h
+++ Raster To ASCII/image_raster.h
@@ -15,11 +15,6 @@ typedef struct {
     size_t current;
 } image;
 
-typedef struct {
-    image* img;
-    int current_x;
-    int current_y;
-} raster_data;
 
 char ascii_by_brightness[] = "`.-':_,^=;><+!rc*/z?sLTv)J7(|Fi{C}fI31tlu[neoZ5Yxjya]2ESwqkP6h9d4VpOGbUAKXHm8RD#$Bg0MNWQ%&@";
 
@@ -53,31 +48,42 @@ static void get_rgba(int pixel, unsigned char* r, unsigned char* g, unsigned cha
 }
 
 static void set_current_pixel(image* img, int x, int y) {
-    img->current = y*img->width*img->channels + x*img->channels;
+    img->current = y*img->width + x;
 }
 
-static int get_pixel(image* img) {
+static void get_current_pixel_xy(image* img, int* x, int* y) {
+    *y = img->current / img->width;
+    *x = img->current - (*y) * img->width;
+}
+
+static int get_pixel_advance(image* img) {
     assert(img->channels <= 4, "Cant convert image with more than 4 channels");
     int pixel = 0;
     for(int i = 0; i < img->channels; i++) {
-        pixel += ((int)*(img->data+img->current+i)) << (8*i);
+        pixel += ((int)*(img->data+(img->current+i)*img->channels)) << (8*i);
     }
     if(img->channels < 4) {
         pixel += 255; //make opaque if no alpha channel
     }
-    img->current += img->channels;
+    img->current++;
     return pixel;
 }
 
-static void read_pixels(image* img, int* buf, int x, int y, int count) {
-    set_current_pixel(img, x, y);
+static void read_pixels(image* img, int* buf, int count) {
     for(int i=0; i < count; i++) {
-        buf[i] = get_pixel(img);
+        buf[i] = get_pixel_advance(img);
     }
 }
-static void read_pixels2d(image* img, int** buf, int x, int y, int count_x, int count_y) {
+static void read_pixels2d(image* img, int** buf, int count_x, int count_y) {
+    size_t current = img->current;
+    int x, y;
+    get_current_pixel_xy(img, &x, &y);
     for(int j=0; j < count_y; j++) {
-        read_pixels(img, buf[j], x, y+j, count_x);
+        img->current = current + img->width * j; //add row
+        read_pixels(img, buf[j], count_x);
+    }
+    if(x + count_x < img->width) {
+        img->current = current + count_x;
     }
 }
 
@@ -90,31 +96,28 @@ float get_brightness(int pixel, int channels) {
 }
 
 // res == 1
-static char get_char(raster_data* rd) {
-    rd->current_x = (rd->current_x+1)%rd->img->width;
-    float brightness = get_brightness(get_pixel(rd->img), rd->img->channels);
+static char get_char(image* img) {
+    float brightness = get_brightness(get_pixel_advance(img), img->channels);
     int ascii_count = sizeof(ascii_by_brightness)/sizeof(ascii_by_brightness[0]) - 1;
 	return ascii_by_brightness[(int)(brightness*(ascii_count-1))];
 }
 
 // res > 1
-static char get_char_w(raster_data* rd, int** buf, int res) {
-    int dist_to_right = rd->img->width - (rd->current_x+res);
-    int dist_to_bottom = rd->img->height - (rd->current_y+res);
+static char get_char_w(image* img, int** buf, int res) {
+    int x, y;
+    get_current_pixel_xy(img, &x, &y);
+    int dist_to_right = img->width - (x+res);
+    int dist_to_bottom = img->height - (y+res);
     int count_x = res - min(dist_to_right, 0);
     int count_y = res - min(dist_to_bottom, 0);
-    read_pixels2d(rd->img, buf, rd->current_x, rd->current_y, count_x, count_y);
-    if(dist_to_right > 0) {
-        count_y = 0;
-    }
-    rd->current_y = (rd->current_y+count_y)%rd->img->height;
-    rd->current_x = (rd->current_x+count_x)%rd->img->width;
+
+    read_pixels2d(img, buf, count_x, count_y);
 
     float brightness = 0;
 
     for(int j=0;j<res;j++) {
         for(int i=0; i<res; i++) {
-            brightness += get_brightness(buf[j][i], rd->img->channels);
+            brightness += get_brightness(buf[j][i], img->channels);
         }
     }
 
@@ -125,14 +128,13 @@ static char get_char_w(raster_data* rd, int** buf, int res) {
 
 
 static void write_raster_to_file(image* img, FILE* file, int res) {
-    raster_data rd = {img, 0, 0};
     char* line_buf;
 	if(res == 1) { 
         line_buf = malloc((img->width + 1) * sizeof(char));
         line_buf[img->width] = '\0';
         for(int j=0;j<img->height;j+=1) {
             for(int i=0;i<img->width;i+=1) {
-                line_buf[i] = get_char(&rd);
+                line_buf[i] = get_char(img);
             }
             fprintf(file, "%s\n", line_buf);
         }
@@ -148,7 +150,7 @@ static void write_raster_to_file(image* img, FILE* file, int res) {
         line_buf[line_buf_len] = '\0';
         for(int j=0;j<y_len;j+=1) {
             for(int i=0;i<x_len;i+=1) {
-                line_buf[i] = get_char_w(&rd, buf, res);
+                line_buf[i] = get_char_w(img, buf, res);
             }
             fprintf(file, "%s\n", line_buf);
         }
@@ -170,7 +172,7 @@ int raster_to_ascii(char* image_name, char* file_out_name, int resolution) {
         return -1;
     }
 
-    if(strcmp(file_out_name, "") == 0) {
+    if(strcmp(file_out_name, "") == 0 || strcmp(file_out_name, image_name) == 0) {
         int img_name_len = strlen(image_name);
         const char name_postfix[] = ".out.txt";
         int name_postfix_len = strlen(name_postfix);
@@ -178,6 +180,7 @@ int raster_to_ascii(char* image_name, char* file_out_name, int resolution) {
         strcpy_s(file_out_name, img_name_len + 1, image_name);
         strcpy_s(file_out_name + img_name_len, name_postfix_len + 1, name_postfix);
     }
+
     FILE* file_out = fopen(file_out_name, "w");
     free(file_out_name);
 
--- Raster To ASCII/main.c
+++ Raster To ASCII/main.c
@@ -6,6 +6,15 @@
 
 
 int main(int argc, char** argv) {
-	raster_to_ascii("image.jpg", "", 1);
+	char* image_name = "image.jpg";
+	char* file_out_name = "";
+	int resolution = 5;
+	if(argc > 1) {
+		image_name = argv[1];
+		if(argc > 2) {
+			resolution = atoi(argv[2]);
+		}
+	}
+	raster_to_ascii(image_name, file_out_name, resolution);
 	return 0;
 }
